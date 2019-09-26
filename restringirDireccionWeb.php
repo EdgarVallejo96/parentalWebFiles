6ce3cd34c1ff30321256d61a12d92e292ad806fb
EdgarVallejo96 <e-emilio@hotmail.com>
2019-09-26 12:15:31 -0600

    Cambio de nombre de tabla y campo.

--- restringirDireccionWeb.php
+++ restringirDireccionWeb.php
@@ -1,7 +1,7 @@
 <?php
     require "conexion.php";
     $app = $_POST["app"];
-    $restriccion = "insert into aplicaciones(nombre_paquete) values('$app');";
+    $restriccion = "insert into paginas_web(direccion_web) values('$app');";
     //$consulta = "update aplicaciones set nombre_paquete = '$app';";
     $resultado = mysqli_query($conexion, $restriccion);
     if($resultado) {
